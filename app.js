64c4dbaa6a650820e673f26f578c57b5cea59c7b
Visavilwa Daisy <135525440+visavilwa1@users.noreply.github.com>
2023-12-14 14:28:54 +0300

    update

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
 
-document.querySelectorAll('.navbar a').forEach(link => {
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-};
-
-window.onscroll = () => {
-  menu.classList.remove("bx-x");
-  navbar.classList.remove("active");
-};
+  menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  };
+   window.onscroll = () => {
+     menu.classList.remove("bx-x");
+     navbar.classList.remove("active");
+   };
